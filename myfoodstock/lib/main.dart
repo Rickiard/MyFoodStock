32dc8f4814668207415ab8c1659ae12fe0a44749
RICARDO DANIEL DA COSTA TEIXEIRA <al79194@alunos.utad.pt>
2025-05-25 15:36:14 +0100

    Revert "Modo fullscreen;"
    This reverts commit 1c2e6858a330fab4368efeb1f34d45344ebe80cd.

--- myfoodstock/lib/main.dart
+++ myfoodstock/lib/main.dart
@@ -1,20 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'screens/home_screen.dart';
 import 'services/storage_service.dart';
 import 'theme/app_theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
-  // Configuração para modo fullscreen com suporte a notch
-  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
-  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-    statusBarColor: Colors.transparent,
-    systemNavigationBarColor: Colors.transparent,
-    systemNavigationBarDividerColor: Colors.transparent,
-  ));
-  
   await StorageService.init();
   runApp(const MyFoodStockApp());
 }
